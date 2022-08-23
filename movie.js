38c01b7f78c75c72b2e5477d29aa8772ab547ba4
2SooBin <109647256+2SooBin@users.noreply.github.com>
2022-08-23 23:48:45 +0900

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,34 +1,35 @@
-const imageOne = document.querySelector('.one');
-const imageTwo = document.querySelector('.two');
-const imageThree = document.querySelector('.three');
-const imageFour = document.querySelector('.four');
+const imageOne = document.querySelector('.img1');
+const imageTwo = document.querySelector('.img2');
+const imageThree = document.querySelector('.img3');
+const imageFour = document.querySelector('.img4');
 
 function blurEffectOne(){
-    document.querySelector('.one').style.filter="blur(5px)";
+    document.querySelector('.img1').style.filter="blur(5px)";
+
 }
 function noneBlurOne(){
-    document.querySelector('.one').style.filter="none"
+    document.querySelector('.img1').style.filter="none";
 }
 
 function blurEffectTwo(){
-    document.querySelector('.two').style.filter="blur(5px)"
+    document.querySelector('.img2').style.filter="blur(5px)"
 }
 function noneBlurTwo(){
-    document.querySelector('.two').style.filter="none"
+    document.querySelector('.img2').style.filter="none"
 }
 
 function blurEffectThree(){
-    document.querySelector('.three').style.filter="blur(5px)"
+    document.querySelector('.img3').style.filter="blur(5px)"
 }
 function noneBlurThree(){
-    document.querySelector('.three').style.filter="none"
+    document.querySelector('.img3').style.filter="none"
 }
 
 function blurEffectFour(){
-    document.querySelector('.four').style.filter="blur(5px)"
+    document.querySelector('.img4').style.filter="blur(5px)"
 }
 function noneBlurFour(){
-    document.querySelector('.four').style.filter="none"
+    document.querySelector('.img4').style.filter="none"
 }
 
 
@@ -45,3 +46,35 @@ imageThree.addEventListener('mouseout', noneBlurThree);
 
 imageFour.addEventListener('mouseover', blurEffectFour);
 imageFour.addEventListener('mouseout', noneBlurFour);
+
+$(document).ready(function(){
+
+    $(".more1").click(function(){
+        $(".pop-up-box1").addClass("active");
+        $(".pop-up-bg").addClass("active");
+    });
+
+    $(".more2").click(function(){
+        $(".pop-up-box2").addClass("active");
+        $(".pop-up-bg").addClass("active");
+    });
+    $(".more3").click(function(){
+        $(".pop-up-box3").addClass("active");
+        $(".pop-up-bg").addClass("active");
+    });
+    $(".more4").click(function(){
+        $(".pop-up-box4").addClass("active");
+        $(".pop-up-bg").addClass("active");
+    });
+
+    
+    $(".btn-clo").click(function(){
+        $(".pop-up-box1").removeClass("active");
+        $(".pop-up-box2").removeClass("active");
+        $(".pop-up-box3").removeClass("active");
+        $(".pop-up-box4").removeClass("active");
+        $(".pop-up-bg").removeClass("active");
+
+
+    });
+})
